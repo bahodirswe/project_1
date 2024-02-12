819c150874c54f97203e6ca54f6fe7f9355204ae
bahodirswe <bahodir01072006@gmail.com>
2024-02-12 16:01:31 +0500

    deployment

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from blog import views
 
 app_name = "blog"
 
 urlpatterns = [
-    
+    path("", views.home,name="home"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls", nemaspace="blog"),)
+    path("", include("blog.urls", namespace="blog"),)
 ]
 
 if settings.DEBUG:
