846e376cba5fd882b5d8d7d9c55454f3d82e1a6f
bahodirswe <bahodir01072006@gmail.com>
2024-04-03 17:51:28 +0500

    adds
    adds

--- core/cdn/conf.py
+++ core/cdn/conf.py
@@ -3,7 +3,7 @@ import os
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_STORAGE_BUCKET_NAME = ''
+AWS_STORAGE_BUCKET_NAME = 'newblog-spaces'
 AWS_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
 AWS_ENDPOINT = "fra1.digitaloceanspaces.com"
 AWS_S3_OBJEKT_PARAMETERS = {
